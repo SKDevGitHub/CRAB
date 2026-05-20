5f64367bbb511a867d36d91541904592aa26b330
Eliot <eaknxp@umsystem.edu>
2026-05-20 15:48:13 -0500

    changed module directory structure to organize imports

---
+++ sim_and_algo_code/JanusVLN/CRAB/algo/JanusVLN/__init__.py
@@ -0,0 +1,15 @@
+class JanusVLN:
+    def get_docs():
+        return """
+        --JanusVLN--
+
+        A vision-language-navigation model.
+
+        Project Page:    https://miv-xjtu.github.io/JanusVLN.github.io/
+        GitHub:          https://github.com/MIV-XJTU/JanusVLN
+        Research Paper:  https://arxiv.org/abs/2509.22548
+        Dataset:         https://www.modelscope.cn/datasets/misstl/JanusVLN_Trajectory_Data
+        Models:          https://www.modelscope.cn/models/misstl/JanusVLN_Base
+                         https://www.modelscope.cn/models/misstl/JanusVLN_Extra
+        """
+    
